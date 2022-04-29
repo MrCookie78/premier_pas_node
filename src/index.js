56d8f413b19ccdc8316111736f349f06b3d76166
Micaël Dupuis <micaeldupuis@live.fr>
2022-04-29 10:01:16 +0200

    Ajout d'un méthode alternative pour décoder le résultat de readFileSync() dans src/index.js pour l'exo 4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 const file = fs.readFileSync(path.join(__dirname, '..', 'assets', 'index.html'), 'utf8');
-const fileLog = fs.readFileSync(path.join(__dirname, 'log.txt'), 'utf8');
+const fileLog = String(fs.readFileSync(path.join(__dirname, 'log.txt')));
 
 console.log(file)
 console.log(fileLog)
