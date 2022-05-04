04cb2455030b9a9e3c1ba01de1e4a5607cf369bb
Micaël Dupuis <micaeldupuis@live.fr>
2022-05-04 16:04:57 +0200

    Ajout du serveur réécrit avec express

---
+++ exo11/server_express.js
@@ -0,0 +1,74 @@
+// const fs = require('fs');
+// const path = require('path');
+const express = require('express')
+const server = express()
+const db = require('./db')
+
+server.use(express.json());
+
+server.get('/api/names', (req, res) => {
+	res.json(Object.fromEntries(db.memoryDb))
+})
+
+server.post('/api/names', (req, res) => {
+	data = req.body;
+
+	if ('name' in data) {
+		let id = db.id;
+		db.id++;
+		db.memoryDb.set(id, data);
+		res.status(201).json({id})
+	}
+
+	else{
+		res.status(400).json({error: '400 - No data received'})
+	}
+})
+
+
+server.get('/api/name/:id', (req, res) => {
+	let id = parseInt(req.params.id);
+
+	if (db.memoryDb.has(id)){
+		object = db.memoryDb.get(id);
+		res.status(200).json(object);
+	}
+	else{
+		res.status(404).json({error: '404 - Data not found'});
+	}
+})
+
+server.put('/api/name/:id', (req, res) => {
+	let id = parseInt(req.params.id);
+	if (db.memoryDb.has(id)){
+		data = req.body;
+
+		if ('name' in data) {
+			let id = db.id;
+			db.id++;
+			db.memoryDb.set(id, data);
+			res.status(201).json({id})
+		}
+
+		else{
+			res.status(400).json({error: '400 - No data received'})
+		}
+	}
+	else{
+		res.status(404).json({error: '404 - Data not found'});
+	}
+})
+
+server.delete('/api/name/:id', (req, res) => {
+	let id = parseInt(req.params.id);
+	if (db.memoryDb.has(id)){
+		db.memoryDb.delete(id);
+		res.status(201).json({id})
+	}
+	else{
+		res.status(404).json({error: '404 - Data not found'});
+	}
+})
+
+module.exports = server;
+
